43fa6704f8f9dc283f77ed6a48c9dbc8151f14f7
Javier Alvarez <javier@crowsoft.com.ar>
2020-09-22 19:45:42 -0300

    first commit

---
+++ src/main/kotlin/ar/com/crowsoft/fonderos/FonderosApplication.kt
@@ -0,0 +1,11 @@
+package ar.com.crowsoft.fonderos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FonderosApplication
+
+fun main(args: Array<String>) {
+    runApplication<FonderosApplication>(*args)
+}
